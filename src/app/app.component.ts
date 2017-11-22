2056f23300f71f73b8ac123c00fcad30aa886080
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-11-22 17:02:22 +0300

    add functionality to display components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{ Details}  from "./details";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Meal Tracker';
+  public masterFoodDisplay: Details[]=[];
+  newInput(newInputFromChild: Details){
+    this.masterFoodDisplay.push(newInputFromChild)
+  }
 }
--- src/app/food-display/food-display.component.ts
+++ src/app/food-display/food-display.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import{ Details}  from "../details";
 @Component({
   selector: 'app-food-display',
   templateUrl: './food-display.component.html',
   styleUrls: ['./food-display.component.css']
 })
 export class FoodDisplayComponent implements OnInit {
+  @Input()childFoodDisplay: Details[];
+
 
   constructor() { }
 
